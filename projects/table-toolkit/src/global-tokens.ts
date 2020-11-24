83e1e5bb21b981fe4ff35a13cc2b88cff2003a4f
TKul6 <123456>
2020-11-24 18:24:53 +0200

    fixup

--- projects/table-toolkit/src/global-tokens.ts
+++ projects/table-toolkit/src/global-tokens.ts
@@ -1,4 +1,5 @@
 import { InjectionToken } from '@angular/core';
 
+/* An injection token to configure wether directives should emits warnings and errors.*/
 export const ANGULAR_TOOLKIT_EMIT_ISSUES =
   new InjectionToken<boolean>('Whether the module should emit warnings and errors to the console.');

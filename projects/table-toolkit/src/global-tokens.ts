4adcba664b754847056754a3a9bedfed82b03be9
TKul6 <123456>
2020-11-29 20:48:06 +0200

    test 2.1

--- projects/table-toolkit/src/global-tokens.ts
+++ projects/table-toolkit/src/global-tokens.ts
@@ -4,4 +4,4 @@ import { InjectionToken } from '@angular/core';
 export const ANGULAR_TOOLKIT_EMIT_ISSUES =
   new InjectionToken<boolean>('Whether the module should emit warnings and errors to the console.');
 
-export const TEMP = 'temp';
+export const TEMP = 'temp 2.1';
